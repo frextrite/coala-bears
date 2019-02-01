5045a0601383148ecce35b0f3f1a9eb5c60e8c48
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-02-01 20:32:50 +0530

    Fix gometalinter config

--- .ci/deps.go.sh
+++ .ci/deps.go.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-go get -u github.com/alecthomas/gometalinter
+curl -L https://git.io/vp6lP | sh
 
-gometalinter --install
-
-go get -u github.com/BurntSushi/toml/cmd/tomlv
+gometalinter --disable-all --enable=deadcode --enable=dupl --enable=errcheck --enable=gochecknoglobals --enable=gochecknoinits --enable=goconst --enable=gocyclo --enable=goimports --enable=golint --enable=gosec --enable=gotype --enable=gotypex --enable=ineffassign --enable=interfacer --enable=lll --enable=maligned --enable=misspell --enable=nakedret --enable=safesql --enable=staticcheck --enable=structcheck --enable=unconvert --enable=unparam --enable=varcheck
